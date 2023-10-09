687e555e71f7c20989d49854118b0506a6ffe8be
ST10145498 <ST10145498@vcconnect.edu.za>
2023-10-09 14:27:59 +0200

    InitialCommit

---
+++ src/practice/pkg1/Practice1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice.pkg1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author USER
+ */
+public class Practice1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"pop up add");
+        JOptionPane.showMessageDialog(null,"kyle");
+        JOptionPane.showMessageDialog(null,"is");
+        JOptionPane.showMessageDialog(null,"cool");
+        JOptionPane.showMessageDialog(null,"hi");
+        JOptionPane.showMessageDialog(null,"super");
+        JOptionPane.showMessageDialog(null,"smile");
+        
+        System.out.println("hello world");
+        
+        
+        
+        
+    }
+    
+}
